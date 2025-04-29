e6d2c0a623168b4ac24bca18103ee7ebcddbcca1
butur-lin <ua-71@ukr.net>
2025-04-29 11:26:09 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{html,js,jsx,ts,tsx}', // Указываем путь к вашим файлам
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
